396d2a16d6615e87735c26f0ddb1a41b8e1ce687
Andrii <melnikandrij070@gmail.com>
2018-12-07 10:57:55 +0200

    add lab 5

---
+++ src/labor5/Main.java
@@ -0,0 +1,19 @@
+package labor5;
+
+public class Main {
+    public static void main(String[] args) {
+        String s =
+                "Внимание. Предварительный просмотр слайдов используется исключительно в ознакомительных целях и может не давать представления о всех возможностях презентации. Если вас заинтересовала данная работа, пожалуйста, загрузите полную версию.";
+        Text text = new Text(s);
+        System.out.println(text);
+        System.out.println("+++++++++++++++++++++++++++++++");
+
+        text.sortedSentencesByWordsCount();
+
+
+
+    }
+}
+
+
+
---
+++ src/labor5/Punctuation.java
@@ -0,0 +1,11 @@
+package labor5;
+public class Punctuation extends SentenceMember{
+    private String symbols;
+
+    public Punctuation(String symbols) {
+        this.symbols = symbols;
+    }
+    public String toString() {
+        return symbols;
+    }
+}
